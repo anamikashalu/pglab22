6c9fde2c9cdfa3fd3b25a40e526ff24bdb18f062
anamikashalu <120364078+anamikashalu@users.noreply.github.com>
2023-02-17 11:12:38 +0530

    Delete co5 ex2.py

--- cycle5/co5 ex2.py
+++
@@ -1,8 +0,0 @@
-import csv
-persons=['Lata',22,45),('Anil',21,56),('john',20,60)]
-csvfile=open('persons.csv','w',newline="")
-obj=csv.writer(csvfile)
-obj.writerows(persons)
-obj.close()
-for person in persons:
-    obj.writerow(person)
